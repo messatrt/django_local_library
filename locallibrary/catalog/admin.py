3a5124b90c7fc3c593aca79f93f5e84f444870b6
messatrt <essaessa9068@gmail.com>
2024-05-10 10:35:34 +0530

    nothing

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Language, Book, BookInstance, Genre
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Language)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
